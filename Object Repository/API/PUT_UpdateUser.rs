587ca2dcf1d6ece62846c1a4a36782a1b800a7e4
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-06-03 11:24:25 +0700

    feat: add PUT update user request object

---
+++ Object Repository/API/PUT_UpdateUser.rs
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>PUT_UpdateUser</name>
+   <tag></tag>
+   <elementGuidId>3e1df998-97c5-4d1a-a33f-832eebf78243</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <autoUpdateContent>false</autoUpdateContent>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>true</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n\t\&quot;name\&quot;: \&quot;${name}\&quot;,\n\t\&quot;job\&quot;: \&quot;${job}\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>x-api-key</name>
+      <type>Main</type>
+      <value>${GlobalVariable.apiKey}</value>
+      <webElementGuid>378f7cab-5e80-4749-a477-44be97a2c78c</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>c6ac4a31-4b7f-40bd-a850-743ab587cc10</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>10.2.0</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <path></path>
+   <restRequestMethod>PUT</restRequestMethod>
+   <restUrl>${GlobalVariable.baseUrl}/users/${userId}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>'Dimas Wahyu'</defaultValue>
+      <description></description>
+      <id>9c7fff1a-3c6f-4edc-90db-6ebccae26c85</id>
+      <masked>false</masked>
+      <name>name</name>
+   </variables>
+   <variables>
+      <defaultValue>'Senior Software Engineer'</defaultValue>
+      <description></description>
+      <id>a0940747-4ab8-4b7e-84e5-b37b6bf78662</id>
+      <masked>false</masked>
+      <name>job</name>
+   </variables>
+   <variables>
+      <defaultValue>GlobalVariable.idUser</defaultValue>
+      <description></description>
+      <id>0a927a0d-5b5f-427b-b8ff-d7850fe32378</id>
+      <masked>false</masked>
+      <name>userId</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+// Parse JSON response
+def jsonSlurper = new JsonSlurper()
+def jsonResponse = jsonSlurper.parseText(response.getResponseBodyContent())
+def user = jsonResponse.data
+
+WS.verifyResponseStatusCode(response, 201)
+assertThat(response.getStatusCode()).isEqualTo(201)
+
+assertThat(user.name).isNotNull()
+assertThat(user.name).isEqualTo(${name})
+assertThat(user.job).isNotNull()
+assertThat(user.job).isEqualTo(${name})</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
