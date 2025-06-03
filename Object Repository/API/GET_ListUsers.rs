7f17ede9bc2c18abd5efb8a99d4448d4d1621f8b
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-06-03 10:12:40 +0700

    feat: add GET all users request object

---
+++ Object Repository/API/GET_ListUsers.rs
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>GET_ListUsers</name>
+   <tag></tag>
+   <elementGuidId>fc11db22-c3fa-45da-bddc-36d7bfcf2574</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <autoUpdateContent>true</autoUpdateContent>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>true</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>x-api-key</name>
+      <type>Main</type>
+      <value>${GlobalVariable.apiKey}</value>
+      <webElementGuid>a066600c-12e5-46be-b893-63befc4d264f</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>10.2.0</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <path></path>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>${GlobalVariable.baseUrl}/users?page=${page}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>2</defaultValue>
+      <description></description>
+      <id>baaa95a1-621d-4701-b54a-bfd43b541463</id>
+      <masked>false</masked>
+      <name>page</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+// Parse JSON response
+def jsonSlurper = new JsonSlurper()
+def jsonResponse = jsonSlurper.parseText(response.getResponseBodyContent())
+
+WS.verifyResponseStatusCode(response, 200)
+assertThat(response.getStatusCode()).isEqualTo(200)
+
+assertThat(jsonResponse.page).isEqualTo(${page})
+jsonResponse.data.each { user ->
+	asserThat(user.id).isNotNull()
+	assertThat(user.email).isNotEmpty()
+	assertThat(user.email).contains(&quot;@reqres.in&quot;)
+}</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
